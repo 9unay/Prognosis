8c0a50c0c6c7151c927d9ddb51f8d3388a72e18c
Gunay Hasanova <gunay0330@gmail.com>
2023-05-15 07:45:45 +0000

    save

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 "use strict";
 $(document).ready(function () {
 
+
+
+
+    $(document).on('keypress', function (e) {
+        if (e.which == 13) {
+            let url = `     `
+            async function myweather (){
+                let responce = await fetch(url)
+                let data = await responce.json()
+                
+            }
+        }
+    })
+
+
+
+
+
+
+
+
 });
