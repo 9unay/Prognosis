95e92f118baa70aab7573f5383c90f8585501310
Gunay Hasanova <gunay0330@gmail.com>
2023-05-16 06:10:09 +0000

    save

--- app.js
+++ app.js
@@ -10,11 +10,32 @@ $(document).ready(function () {
             async function myweather (){
                 let responce = await fetch(url)
                 let data = await responce.json()
-                
+                $("#city").html(data.name)
+                $("#temp").html(data.main.temp)
+                $("#feels").html(data.main.feels_like)
+                $("#myWeather").val("")
+                console.log(data)
             }
+            myweather()
         }
     })
 
+    $("#btn").click(function (e) {
+        e.preventDefault()
+        let url = `   `
+        async function myweather() {
+            let responce = await fetch(url)
+            let data = await responce.json()
+            $("#city").html(data.name)
+            $("#temp").html(data.main.temp)
+            $("#feels").html(data.main.feels_like)
+            $("#myWeather").val("")
+            console.log(data)
+        }
+        
+
+
+    })
 
 
 
